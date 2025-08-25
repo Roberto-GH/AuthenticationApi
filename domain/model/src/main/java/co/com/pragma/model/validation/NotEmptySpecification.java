751bc1366708db3b7cf1ffc36164af2a63e52b42
Roberto Londoño <roberthdrums@gmail.com>
2025-08-25 12:29:19 -0500

    swagger doc

--- domain/model/src/main/java/co/com/pragma/model/validation/NotEmptySpecification.java
+++ domain/model/src/main/java/co/com/pragma/model/validation/NotEmptySpecification.java
@@ -1,6 +1,10 @@
 package co.com.pragma.model.validation;
 
+import java.util.logging.Logger;
+
 public class NotEmptySpecification implements Specification<String> {
+
+  private static final Logger LOG = Logger.getLogger(NotEmptySpecification.class.getName());
 
   private final String fieldName;
 
@@ -11,6 +15,7 @@ public class NotEmptySpecification implements Specification<String> {
   @Override
   public void validate(String candidate) throws DomainValidationException {
     if (candidate == null || candidate.trim().isEmpty()) {
+      LOG.severe("Validation failed: The field '" + fieldName + "' cannot be null or empty.");
       throw new DomainValidationException("The field '" + fieldName + "' cannot be null or empty.", 400);
     }
   }
--- infrastructure/entry-points/reactive-web/src/main/java/co/com/pragma/api/RouterAuth.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/pragma/api/RouterAuth.java
@@ -1,9 +1,18 @@
 package co.com.pragma.api;
 
 import co.com.pragma.api.config.AuthPath;
-import co.com.pragma.api.config.UserPath;
+import co.com.pragma.api.dto.LoginDto;
+import co.com.pragma.model.user.Token;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springdoc.core.annotations.RouterOperation;
+import org.springdoc.core.annotations.RouterOperations;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
@@ -14,19 +23,32 @@ import static org.springframework.web.reactive.function.server.RouterFunctions.r
 public class RouterAuth {
 
   private final AuthPath authPath;
-  private final UserPath userPath;
   private final UserHandler userHandler;
 
-  public RouterAuth(AuthPath authPath, UserPath userPath, UserHandler userHandler) {
+  public RouterAuth(AuthPath authPath, UserHandler userHandler) {
     this.authPath = authPath;
-    this.userPath = userPath;
     this.userHandler = userHandler;
   }
 
+  @RouterOperations({
+    @RouterOperation(
+      path = "/auth/v1/login",
+      method = RequestMethod.POST,
+      beanClass = UserHandler.class,
+      beanMethod = "listenLogin",
+      operation = @Operation(
+        operationId = "login", responses = {
+        @ApiResponse(
+          responseCode = "200",
+          description = "Successful operation",
+          content = @Content(mediaType = "application/json", schema = @Schema(implementation = Token.class)))
+      },
+        requestBody = @RequestBody(content = @Content(schema = @Schema(implementation = LoginDto.class)))
+      ))
+  })
   @Bean
   public RouterFunction<ServerResponse> routerFunctionAuth() {
-    return route(POST(authPath.getLogin()), userHandler::listenLogin)
-      .and(route(POST(userPath.getSignUp()), userHandler::listenSaveUser));
+    return route(POST(authPath.getLogin()), userHandler::listenLogin);
   }
 
 }
